70e4075eb028999ff9be7b5d6bee65fdf98ad056
Headstart <skepquilan2@gmaiil.com>
2020-12-01 13:03:32 +0000

    Added Bottom Navigation Bar

--- app/src/main/java/com/example/headstart/HomeActivity.java
+++ app/src/main/java/com/example/headstart/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.example.headstart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,6 +21,17 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                return false;
+            }
+        });
+
 
         btnLogout = findViewById(R.id.logoutBtn);
         btnLogout.setOnClickListener(this);
