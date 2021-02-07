0e5c322d924bc914639df4261db8a0cb0c5776ce
skep <skepquilan2@gmail.com>
2021-02-07 16:41:06 +0000

    Added Driver dailog plus changed graddle build files

--- app/src/main/java/com/example/headstart/Drivers/DriversActivity.java
+++ app/src/main/java/com/example/headstart/Drivers/DriversActivity.java
@@ -2,10 +2,13 @@ package com.example.headstart.Drivers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -16,16 +19,24 @@ import com.example.headstart.Map.MapActivity;
 import com.example.headstart.R;
 import com.example.headstart.Settings.SettingsActivity;
 import com.example.headstart.Trucks.TrucksActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DriversActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener,
         View.OnClickListener {
 
     private BottomNavigationView bottomNavigationView;
-    private FloatingActionButton addDriverFloatBtn, cancelFloatBtn,mapFloatBtn;
+    private FloatingActionButton addDriverFloatBtn;
     private AlertDialog.Builder alertDialogBuilder;
     private AlertDialog alertDialog;
+
+    private FirebaseAuth auth;
 
     private EditText editFirstName, editLastName, editPhoneNumber,
             editEmail, editDriverID, editVehicleID;
@@ -44,9 +55,10 @@ public class DriversActivity extends AppCompatActivity implements BottomNavigati
         addDriverFloatBtn = findViewById(R.id.addDriver_fab);
         addDriverFloatBtn.setOnClickListener(this);
 
-        mapFloatBtn = findViewById(R.id.map_floatBar);
+        FloatingActionButton mapFloatBtn = findViewById(R.id.map_floatBar);
         mapFloatBtn.setOnClickListener(this);
 
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -80,11 +92,11 @@ public class DriversActivity extends AppCompatActivity implements BottomNavigati
     }
 
     /**
-     * Add driver info popUp function
+     * Add driver info Dialog function
      */
     private void addDriverDialog() {
         alertDialogBuilder = new AlertDialog.Builder(this);
-        final View addDriverDialog = getLayoutInflater().inflate(R.layout.activity_add_driver, null);
+        final View addDriverDialog = getLayoutInflater().inflate(R.layout.dialog_add_driver, null);
 
 
         //Text boxes
@@ -100,7 +112,19 @@ public class DriversActivity extends AppCompatActivity implements BottomNavigati
         alertDialog = alertDialogBuilder.create();
         alertDialog.show();
 
-        cancelFloatBtn = addDriverDialog.findViewById(R.id.cancel_float_Btn);
+        //add button
+        addDriverFloatBtn = addDriverDialog.findViewById(R.id.addDriver_float_Btn);
+        addDriverFloatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //perform this function
+                addDriver();
+            }
+        });
+
+        //cancel button
+        FloatingActionButton cancelFloatBtn = addDriverDialog.findViewById(R.id.cancel_float_Btn);
         cancelFloatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,6 +133,84 @@ public class DriversActivity extends AppCompatActivity implements BottomNavigati
         });
 
     }
+
+    /**
+     * adding driver to database
+     */
+    private void addDriver() {
+        String driverFirstName = editFirstName.getText().toString().trim();
+        String driverLastName = editLastName.getText().toString().trim();
+        String driverPhone = editPhoneNumber.getText().toString().trim();
+        String driverEmail = editEmail.getText().toString().trim();
+        String driverID = editDriverID.getText().toString().trim();
+        String vehicleID = editVehicleID.getText().toString().trim();
+
+        //If Statements to validate these Inputs..Check validation
+        if (driverFirstName.isEmpty()) {
+            editFirstName.setError("FirstName is required");
+            editFirstName.requestFocus();
+        }
+        else if (driverLastName.isEmpty()) {
+            editLastName.setError("LastName is required");
+            editLastName.requestFocus();
+        }
+        else if (driverPhone.isEmpty() || (driverPhone.length() < 10)) {
+            editPhoneNumber.setError("Enter a valid Phone Number");
+            editPhoneNumber.requestFocus();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(driverEmail).matches()) {
+            editEmail.setError("Please provide valid email");
+            editEmail.requestFocus();
+        }
+        else if (driverID.isEmpty()) {
+            editDriverID.setError("Driver Id is required");
+            editDriverID.requestFocus();
+        }
+        else if (vehicleID.isEmpty()) {
+            editVehicleID.setError("LastName is required");
+            editVehicleID.requestFocus();
+        }
+
+
+        //add driver info when user clicks add button
+        //Create Driver info with these below
+        final Drivers drivers = new Drivers(
+                driverFirstName,
+                driverLastName,
+                driverPhone,
+                driverEmail,
+                driverID,
+                vehicleID
+        );
+
+        //take driver info to firebase database
+        FirebaseDatabase.getInstance().getReference("User drivers")
+                //the current user's ID is giving as foreign key in drivers table
+                .child(auth.getCurrentUser().getUid()).push()
+                .setValue(drivers)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        editFirstName.setText("");
+                        editLastName.setText("");
+                        editPhoneNumber.setText("");
+                        editEmail.setText("");
+                        editDriverID.setText("");
+                        editVehicleID.setText("");
+                        Toast.makeText(DriversActivity.this,"Added successfully", Toast.LENGTH_LONG).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(DriversActivity.this,"Failed....Try Again", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+
 
     /**
      * Bottom Navigation bar
